b9e197aeec54ae93fb54462e4d50c5cefe910b74
Samuel Scozzari <s_scozzari@libero.it>
2022-01-16 12:55:55 +0100

    added comments

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ new Vue({
   },
 
   methods: {
+    /* se imgIndex è <= 0 allora imgIndex sarà uguale alla lunghezza dell'array di immagini - 1; altrimenti continuerà ad essere decrementata di uno
+  per ogni click del pulsante a cui è associata tale funzione */
     prevImg: function () {
       if (this.imgIndex <= 0) {
         this.imgIndex = this.images.length - 1;
@@ -14,6 +16,7 @@ new Vue({
       }
     },
     nextImg: function () {
+      //funziona in modo analogo alla superiore
       if (this.imgIndex >= this.images.length - 1) {
         this.imgIndex = 0;
       } else {
