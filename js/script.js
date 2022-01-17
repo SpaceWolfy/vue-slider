569d333fe75e52b79a7ebbf0955842b784c203a8
Samuel Scozzari <s_scozzari@libero.it>
2022-01-17 10:16:19 +0100

    added autoplay function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 new Vue({
   el: "#app",
   data: {
+    //imgIndex serve ad indicare l'elemento che dobbiamo mostrare prendendolo da dentro il data
     imgIndex: 0,
     images: ["img1.jpg", "img2.jpg", "img3.jpg"],
+    timer: null,
   },
 
   methods: {
+    /* creare timing function */
+    sliderAutoPlay: function () {
+      this.timer = setInterval(() => {
+        this.nextImg();
+      }, 3000);
+    },
     /* se imgIndex è <= 0 allora imgIndex sarà uguale alla lunghezza dell'array di immagini - 1; altrimenti continuerà ad essere decrementata di uno
   per ogni click del pulsante a cui è associata tale funzione */
     prevImg: function () {
@@ -24,4 +32,10 @@ new Vue({
       }
     },
   },
+  //fa funzionare l'autoplay soltanto quando tutto l'html è stato stampato in pagina
+  // il mounted e il created vengono richiamati da Vue.js stesso e sa lui quando chiamarli, per questo si mettono fuori dal methods
+  mounted: function () {
+    this.sliderAutoPlay();
+  },
+  //mousehover to do
 });
